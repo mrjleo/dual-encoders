4501558490548cf58547101669a9322fcc8a93ba
mrjleo <mrjleo@users.noreply.github.com>
2023-02-11 18:49:56 +0100

    add IRDSPartialCorpusEncodingDataset

--- data/irds.py
+++ data/irds.py
@@ -1,6 +1,8 @@
-from typing import Iterable
+from pathlib import Path
+from typing import Iterable, Union
 
 import ir_datasets
+from ranking_utils.datasets.trec import read_top_trec
 from ranking_utils.model.data import DataProcessor
 
 from data import EncodingDataset, EncodingInstance
@@ -33,5 +35,39 @@ class IRDSCorpusEncodingDataset(EncodingDataset):
 
     def _get_data(self) -> Iterable[EncodingInstance]:
         for index, doc in enumerate(self.dataset.docs_iter()):
+            content = [getattr(doc, attr) for attr in self.content_attributes]
+            yield index, ". ".join(content)
+
+
+class IRDSPartialCorpusEncodingDataset(IRDSCorpusEncodingDataset):
+    """Encoding dataset for corpora from the ir_datasets library.
+    Encodes only documents that appear in a provided TREC runfile."""
+
+    def __init__(
+        self,
+        data_processor: DataProcessor,
+        dataset: str,
+        trec_runfile: Union[Path, str],
+        content_attributes: Iterable[str],
+        max_len: int = None,
+    ) -> None:
+        """Constructor.
+
+        Args:
+            data_processor (DataProcessor): A model-specific data processor.
+            dataset (str): Dataset identifier within ir_datasets (must have a corpus).
+            trec_runfile (Union[Path, str]): Only documents that appear in this TREC runfile will be yielded.
+            content_attributes (Iterable[str]): Attributes of the document representation to use as content (dataset-specific).
+            max_len (int, optional): Split long documents into chunks (length in characters). Defaults to None.
+        """
+        super().__init__(data_processor, dataset, content_attributes, max_len)
+
+        # we support Path and str to make config with hydra easier
+        self.doc_ids = set.union(*read_top_trec(Path(trec_runfile)).values())
+
+    def _get_data(self) -> Iterable[EncodingInstance]:
+        for index, doc in enumerate(
+            self.dataset.docs_store().get_many_iter(self.doc_ids)
+        ):
             content = [getattr(doc, attr) for attr in self.content_attributes]
             yield index, ". ".join(content)
