fix indexing script not respecting projection layer and normalization

index.py
@@ -45,11 +45,7 @@ def main(config: DictConfig) -> None:
     t_index.start()
 
     for ids, d_inputs in tqdm(data_loader):
-        with torch.no_grad():
-            out = doc_encoder.encoder(
-                {k: v.to(config.device) for k, v in d_inputs.items()}
-            )
-        q.put((ids, out.detach().cpu().numpy()))
+        q.put((ids, doc_encoder._encode(d_inputs)))
 
     # sentinel
     q.put(None)

util/__init__.py
@@ -100,11 +100,20 @@ class StandaloneEncoder(FFEncoder):
             self.projection = None
         self.encoder.eval()
 
-    def __call__(self, texts: Sequence[str]) -> np.ndarray:
+    def _encode(self, inputs: Dict[str, torch.Tensor]) -> torch.Tensor:
+        """Encode and normalize tokenized inputs.
+
+        Args:
+            inputs (Dict[str, torch.Tensor]): The tokenized inputs.
+
+        Returns:
+            torch.Tensor: The normalized representations.
+        """
         with torch.no_grad():
-            rep = self.encoder(
-                {k: v.to(self.device) for k, v in self.tokenizer(texts).items()}
-            )
+            rep = self.encoder({k: v.to(self.device) for k, v in inputs.items()})
             if self.projection is not None:
                 rep = self.projection(rep)
             return torch.nn.functional.normalize(rep).detach().numpy()
+
+    def __call__(self, texts: Sequence[str]) -> np.ndarray:
+        return self._encode(self.tokenizer(texts))